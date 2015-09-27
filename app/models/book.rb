90b55ed689898641822747a35ca8495a8819adee
toshiemon18 <st12318@gmail.com>
2015-09-27 18:58:19 +0900

    ファーストコミット

---
+++ app/models/book.rb
@@ -0,0 +1,34 @@
+class Book < ActiveRecord::Base
+  enum status: %w{reservation now_on_sale end_of_print}
+
+	scope :costly, -> {where("price > ?", 3000)}
+	scope :written_about, ->(theme) {where("name like ?", "%#{theme}%")}
+
+	belongs_to :publisher
+	
+	has_many :book_authors
+	has_many :authors, through: :book_authors
+
+	validates :name, presence: true
+	validates :name, length: { maximum: 15 }
+	validates :price, numericality: { greater_than_or_equal_to: 0 }
+	validate do |book|
+		if book.name.include?("exercise") then
+			book.errors[:name] << "I don't like exercise."
+		end
+	end
+  before_validation do |book|
+    book.name = self.name.gsub(/Cat/) { |matched| "lovely #{matched}" }
+  end
+
+  after_destroy {|book| Rails.logger.info("Book is deleted: #{book.attributes.inspect}") }
+
+  def high_price?
+    price >= 5000
+  end
+
+  after_destroy :if => :high_price? do |book|
+    Rails.logger.warn "Book with high price is deleted: #{book.attributes.inspect}"
+    Rails.logger.warn "Please Check!!"
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,53 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20150927085933) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "name"
+    t.string   "penname"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "book_authors", force: :cascade do |t|
+    t.integer  "book_id"
+    t.integer  "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "book_authors", ["author_id"], name: "index_book_authors_on_author_id"
+  add_index "book_authors", ["book_id"], name: "index_book_authors_on_book_id"
+
+  create_table "books", force: :cascade do |t|
+    t.string   "name"
+    t.date     "published_on"
+    t.integer  "price"
+    t.integer  "number_of_page"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.integer  "publisher_id"
+    t.integer  "status"
+  end
+
+  add_index "books", ["publisher_id"], name: "index_books_on_publisher_id"
+
+  create_table "publishers", force: :cascade do |t|
+    t.string   "name"
+    t.text     "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end

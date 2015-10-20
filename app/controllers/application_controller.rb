ef23e166dffbec8805e8aa377007a9de842c2aff
toshiemon18 <st12318@gmail.com>
2015-10-20 23:43:19 +0900

    Perfect RoR P85

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  before_action :detect_mobile_viriant
+
   protect_from_forgery with: :exception
+  private
+  def detect_mobile_viriant
+  	request.variant = :mobile if request.user_agent =~ /iPhone/
+  end
 end
